5f65f31f6c4084d9e0adbd9b35671ef4da3216c2
Mark Schmidt <mark.schmidt@hey.com>
2023-07-07 22:36:25 -0500

    Better teams

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ extern crate prost_build;
 
 fn main() {
     let mut config = prost_build::Config::new();
-    config.type_attribute("Sport", "#[derive(Eq, Hash, Copy, serde::Deserialize)]");
-    config.type_attribute(".", "#[derive(serde::Serialize)]");
+    config.type_attribute("Sport", "#[derive(Eq, Hash, Copy)]");
+    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
     config
         .compile_protos(&["src/common/types.proto"], &["src/"])
         .unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate maplit;
-
 pub mod common;
 mod sport;
 
